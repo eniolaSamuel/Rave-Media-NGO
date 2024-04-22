9d8b50c89fe2c8f3293112bedab8dd253056b3b4
Eniola Samuel <theeniolasamuel@gmail.com>
2024-04-22 18:34:20 +0100

    Subsequent commit

---
+++ src/views/About.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Lottie from 'lottie-react'
+import "../styles/AbouUs.css"
+
+
+export const AboutUs = () => {
+    return(
+        <div className="about-us-frame">
+            <div className="about-us-sub-frame">
+                <div className="about-us-header-frame">
+                    <h1>ABOUT US</h1>
+                    <hr/>
+                </div>
+                <div className="about-us-sub-frame-content">
+                    
+                </div>
+
+            </div>
+        </div>
+    )
+}
--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import HomeImage from "../assets/image/svg/home-Page-Background.svg"
+import DonationBox from "../assets/icons/svg/donate-icon.svg"
+import VolunteerHands from "../assets/icons/svg/volunteer-icon.svg"
 import "../styles/HomePage.css"
 
 export const HomePage = () => {
@@ -13,7 +15,12 @@ export const HomePage = () => {
                         we empower kids to build brighter futures. Join us in making a difference today.</p>
                 </div>
                 <div className="text-button">
-                    <button className="sign-up-button-big"> sign up </button>
+                    <button className="sign-up-button-big"> donate now
+                        <img src={DonationBox} alt={DonationBox}/>
+                    </button>
+                    <button className="sign-up-button-big"> be a volunteer
+                        <img src={VolunteerHands} alt={VolunteerHands}/>
+                    </button>
                 </div>
             </div>
 
